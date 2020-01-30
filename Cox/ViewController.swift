7b42fc8ec13ae48f31fd3559445996f05d783006
Mihai Chis <mh_chis@yahoo.com>
2020-01-30 16:14:17 +0200

    add ios 12 support

--- Cox/ViewController.swift
+++ Cox/ViewController.swift
@@ -6,15 +6,377 @@
 //  Copyright © 2020 Mihai Chis. All rights reserved.
 //
 
+import AVFoundation
+import Vision
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
+    var captureSession: AVCaptureSession!
+    var backCamera: AVCaptureDevice?
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    var captureOutput: AVCapturePhotoOutput?
+
+    var shutterButton: UIButton!
+
+//    var productCatalog = ProductCatalog()
+
+    lazy var detectBarcodeRequest: VNDetectBarcodesRequest = {
+        return VNDetectBarcodesRequest(completionHandler: { (request, error) in
+            guard error == nil else {
+                self.showAlert(withTitle: "Barcode Error", message: error!.localizedDescription)
+                return
+            }
+
+            self.processClassification(for: request)
+        })
+    }()
+
+    // MARK: - View controller life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        checkPermissions()
+        setupCameraLiveView()
+        addShutterButton()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        // Every time the user re-enters the app, we must be sure we have access to the camera.
+        checkPermissions()
+    }
+
+    // MARK: - User interface
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    // MARK: - Camera
+    private func checkPermissions() {
+        let mediaType = AVMediaType.video
+        let status = AVCaptureDevice.authorizationStatus(for: mediaType)
+
+        switch status {
+        case .denied, .restricted:
+            displayNotAuthorizedUI()
+        case.notDetermined:
+            // Prompt the user for access.
+            AVCaptureDevice.requestAccess(for: mediaType) { granted in
+                guard granted != true else { return }
+
+                // The UI must be updated on the main thread.
+                DispatchQueue.main.async {
+                    self.displayNotAuthorizedUI()
+                }
+            }
+
+        default: break
+        }
+    }
+
+    private func setupCameraLiveView() {
+        // Set up the camera session.
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .hd1280x720
+
+        // Set up the video device.
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera],
+                                                                      mediaType: AVMediaType.video,
+                                                                      position: .back)
+        let devices = deviceDiscoverySession.devices
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.back {
+                backCamera = device
+            }
+        }
+
+        // Make sure the actually is a back camera on this particular iPhone.
+        guard let backCamera = backCamera else {
+            showAlert(withTitle: "Camera error", message: "There seems to be no camera on your device.")
+            return
+        }
+
+        // Set up the input and output stream.
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: backCamera)
+            captureSession.addInput(captureDeviceInput)
+        } catch {
+            showAlert(withTitle: "Camera error", message: "Your camera can't be used as an input device.")
+            return
+        }
+
+        // Initialize the capture output and add it to the session.
+        captureOutput = AVCapturePhotoOutput()
+        captureOutput!.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+        captureSession.addOutput(captureOutput!)
+
+        // Add a preview layer.
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        cameraPreviewLayer!.videoGravity = .resizeAspectFill
+        cameraPreviewLayer!.connection?.videoOrientation = .portrait
+        cameraPreviewLayer?.frame = view.frame
+
+        self.view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+
+        // Start the capture session.
+        captureSession.startRunning()
+    }
+
+    @objc func captureImage() {
+        let settings = AVCapturePhotoSettings()
+        captureOutput?.capturePhoto(with: settings, delegate: self)
+    }
+
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let imageData = photo.fileDataRepresentation(),
+            let image = UIImage(data: imageData) {
+
+            // Convert image to CIImage.
+            guard let ciImage = CIImage(image: image) else {
+                fatalError("Unable to create \(CIImage.self) from \(image).")
+            }
+
+            // Perform the classification request on a background thread.
+            DispatchQueue.global(qos: .userInitiated).async {
+                let handler = VNImageRequestHandler(ciImage: ciImage, orientation: CGImagePropertyOrientation.up, options: [:])
+
+                do {
+                    try handler.perform([self.detectBarcodeRequest])
+                } catch {
+                    self.showAlert(withTitle: "Error Decoding Barcode", message: error.localizedDescription)
+                }
+            }
+        }
+    }
+
+    // MARK: - User interface
+    private func displayNotAuthorizedUI() {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width * 0.8, height: 20))
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.text = "Please grant access to the camera for scanning barcodes."
+        label.sizeToFit()
+
+        let button = UIButton(frame: CGRect(x: 0, y: label.frame.height + 8, width: view.frame.width * 0.8, height: 35))
+        button.layer.cornerRadius = 10
+        button.setTitle("Grant Access", for: .normal)
+        button.backgroundColor = UIColor(displayP3Red: 4.0/255.0, green: 92.0/255.0, blue: 198.0/255.0, alpha: 1)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(openSettings), for: .touchUpInside)
+
+        let containerView = UIView(frame: CGRect(
+            x: view.frame.width * 0.1,
+            y: (view.frame.height - label.frame.height + 8 + button.frame.height) / 2,
+            width: view.frame.width * 0.8,
+            height: label.frame.height + 8 + button.frame.height
+            )
+        )
+        containerView.addSubview(label)
+        containerView.addSubview(button)
+        view.addSubview(containerView)
+    }
+
+    private func addShutterButton() {
+        let width: CGFloat = 75
+        let height = width
+        shutterButton = UIButton(frame: CGRect(x: (view.frame.width - width) / 2,
+                                               y: view.frame.height - height - 32,
+                                               width: width,
+                                               height: height
+            )
+        )
+        shutterButton.layer.cornerRadius = width / 2
+        shutterButton.backgroundColor = UIColor.init(displayP3Red: 1, green: 1, blue: 1, alpha: 0.8)
+        shutterButton.showsTouchWhenHighlighted = true
+        shutterButton.addTarget(self, action: #selector(captureImage), for: .touchUpInside)
+        view.addSubview(shutterButton)
+    }
+
+    private func showInfo(for payload: String) {
+        
+//        if let product = productCatalog.item(forKey: payload) {
+        if payload != "" {
+            print(payload)
+            showAlert(withTitle: "VIN", message: payload)
+        } else {
+            showAlert(withTitle: "No item found for this payload", message: "")
+        }
+    }
+
+    // MARK: - Vision
+    func processClassification(for request: VNRequest) {
+        DispatchQueue.main.async {
+            if let bestResult = request.results?.first as? VNBarcodeObservation,
+                let payload = bestResult.payloadStringValue {
+                self.showInfo(for: payload)
+            } else {
+                self.showAlert(withTitle: "Unable to extract results",
+                               message: "Cannot extract barcode information from data.")
+            }
+        }
+    }
+
+    // MARK: - Helper functions
+    @objc private func openSettings() {
+        let settingsURL = URL(string: UIApplication.openSettingsURLString)!
+        UIApplication.shared.open(settingsURL) { _ in
+            self.checkPermissions()
+        }
+    }
+
+    private func showAlert(withTitle title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alertController, animated: true)
+    }
+}
+/*
+import AVFoundation
+import UIKit
+import OSXScanLib
+
+class ViewController: ScanViewController {
+
+    @IBOutlet weak var vinLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        vinLabel.text = "";
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        super.setLaserRendererClass(DataMatrixLaserRenderer.self)
+        super.resumeScanning()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func back() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func setResult(_ _vin: String) {
+        vinLabel.text = _vin;
+    }
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+        return UIInterfaceOrientation.landscapeRight
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.landscapeRight
+    }
+}
+ */
+ 
+/*
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = UIColor.black
+        captureSession = AVCaptureSession()
+
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed()
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417] //, .code39, .dataMatrix
+        } else {
+            failed()
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+
+        captureSession.startRunning()
     }
 
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning()
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+
+        dismiss(animated: true)
+    }
+
+    func found(code: String) {
+        print(code)
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
 }
-
+*/
